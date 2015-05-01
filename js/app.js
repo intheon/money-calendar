0a243ff7edbce250aada98424d62b3c00ff843ac
intheon <allobon@gmail.com>
2015-05-01 11:57:07 +0100

    tidied

--- js/app.js
+++ js/app.js
@@ -1,13 +1,80 @@
-$(document).ready(function(){
-	var time = {};
-	var money = {};
-	var rootDir = "http://localhost/money-calendar/";
-	//var rootDir = "http://intheon.xyz/liv/";
+// app.js - the 'glue' - 2015 intheon
+
+// -----------------------------------------------------------------------------------
+// 		Workflow
+//
+// 	-	Draw a blank calendar with moment.js
+// 	-	Check if net wage for that month is available. if it is retreive and draw it, if it isnt request it, store it, retreive, and draw
+// 	-	Have form to add in spend for that day
+// 	-	Also have it so you can add spend for previous and future days
+// 	-	Have information panel to show how much remains, broken down into useful facts
+// -----------------------------------------------------------------------------------
+
+// GO
+
+// initialisation, fires all the stuff when the page is loaded
+
+$(document).ready(function()
+{
+	// globals
+	var time 			= {};
+	var money 			= {};
+	var rootDir 		= "http://localhost/money-calendar/"; // local
+	//var rootDir 		= "http://intheon.xyz/liv/"; // production
+
+	// functions
 	defineMetadata(time);
 	checkApplicationLogic(rootDir,time);
 	loadCalendar(time);
 	loadInformation(time);
 });
+
+// defining some useful stuff in objects, as a tidier shorthand
+
+function defineMetadata(time,money)
+{
+	time.today 			= moment();
+	time.year 			= moment().year();
+	time.month 			= moment().format("MMMM");
+	time.todaysDate 	= moment().format("D");
+	time.daysInMonth 	= moment().daysInMonth();
+	time.payday 		= moment().date("28");
+	time.toPayday 		= time.payday.diff(time.today,"days");
+}
+
+// actually draws the calendar to the dom
+
+function loadCalendar(time)
+{
+	// draw all the dates
+	for (counter = 1; counter <= time.daysInMonth; counter++)
+	{
+		$("#cost-calendar").append("<div class='calendar-item' id='calendar-item-"+counter+"'>\
+			<div class='date-number'>"+counter+"</div>\
+			<div class='date-body'></div>\
+			</div>");
+		if (counter == time.todaysDate)
+		{
+			$("#calendar-item-" + counter).append("<div class='label'>Today</div>");
+			$("#calendar-item-" + counter).css("background-color","#FDE7BA");
+		}
+		if (counter == 28)
+		{
+			$("#calendar-item-28").append("<div class='label'>Payday</div>")
+		}
+	}
+}
+
+// and draws all the helpful bits of motovational information
+function loadInformation(time)
+{
+	$("#information-panel").append("<div class='information-row'>\
+			<div class='information-month'>It is "+time.month+"</div>\
+			<div class='information-next-payday'>You have "+time.toPayday+" days until payday</div>\
+		</div>")
+}
+
+
 
 function checkApplicationLogic(rootDir,time)
 {
@@ -34,47 +101,9 @@ var checkIfFileExists = function(rootDir,fileName)
 	});
 }
 
-function defineMetadata(time,money)
-{
-	time.today 			= moment();
-	time.year 			= moment().year();
-	time.month 			= moment().format("MMMM");
-	time.todaysDate 	= moment().format("D");
-	time.daysInMonth 	= moment().daysInMonth();
-	time.payday 		= moment().date("28");
-	time.toPayday 		= time.payday.diff(time.today,"days");
-}
 
 function defineMoney(time,money)
 {
 	money.gross			= "???";
 }
 
-function loadCalendar(time)
-{
-	// draw all the dates
-	for (counter = 1; counter <= time.daysInMonth; counter++)
-	{
-		$("#cost-calendar").append("<div class='calendar-item' id='calendar-item-"+counter+"'>\
-			<div class='date-number'>"+counter+"</div>\
-			<div class='date-body'></div>\
-			</div>");
-		if (counter == time.todaysDate)
-		{
-			$("#calendar-item-" + counter).append("<div class='label'>Today</div>");
-			$("#calendar-item-" + counter).css("background-color","#FDE7BA");
-		}
-		if (counter == 28)
-		{
-			$("#calendar-item-28").append("<div class='label'>Payday</div>")
-		}
-	}
-}
-
-function loadInformation(time)
-{
-	$("#information-panel").append("<div class='information-row'>\
-			<div class='information-month'>It is "+time.month+"</div>\
-			<div class='information-next-payday'>You have "+time.toPayday+" days until payday</div>\
-		</div>")
-}
