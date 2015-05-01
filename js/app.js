1a63e0e5d6fb8fde64dcfa1e2aadb750c0a4143a
intheon <allobon@gmail.com>
2015-05-01 16:27:35 +0100

    moar pdo

--- js/app.js
+++ js/app.js
@@ -125,4 +125,3 @@ function defineMoney(time,money)
 {
 	money.gross			= "???";
 }
-
--- php/money.php
+++ php/money.php
@@ -1,14 +1,18 @@
 <?php
 
+
 // checks if whichever month i am in (from the 28th of the prev month) has any data associated with it.
 
+// globals
 $host 				= "localhost";
 $username 			= "root";
 $password 			= "";
 $dbname 			= "money-calendar";
 
+// only this block if checking for a certain months pay
 if (isset($_POST['monthToCheck']))
 {
+	// assign it to a var
 	$monthNum 		= $_POST['monthToCheck'];
 
 	try
@@ -20,12 +24,18 @@ if (isset($_POST['monthToCheck']))
 		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 		// a statement to retrieve all matching records
-		$statement = $connection->prepare("SELECT * FROM paydays WHERE month_number = $monthNum");
+		$statement = $connection->prepare("SELECT * FROM paydays WHERE month_number = :month");
 
+		// bind parameters
+		$statement->bindParam(':month', $monthNum);
+
+		// execute
 		$statement->execute();
 
+		// get response
 		$response = $statement->fetchAll();
 
+		// cycle through
 		foreach ($response as $row) {
     		echo $row['payday_amount'];
 		}
@@ -38,7 +48,10 @@ if (isset($_POST['monthToCheck']))
 	}
 	catch (PDOException $exception)
 	{
+		// get exception message
 		echo "I'm sorry, Dave. I'm afraid I can't let you do that. | " . $exception->getMessage();
+
+		// write it to a log file
     	file_put_contents('PDOErrors.txt', $exception->getMessage(), FILE_APPEND);
 	}
 
